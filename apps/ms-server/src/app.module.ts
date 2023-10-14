7ec275864411e23faed7c34cdf0eeb9a6166501e
Raymond <akdfhr2@gmail.com>
2023-10-15 00:17:34 +0900

    [SERVER] fix: 배포용 스크립트 변경

--- apps/ms-server/src/app.module.ts
+++ apps/ms-server/src/app.module.ts
@@ -4,9 +4,10 @@ import { DbModule } from './db/db.module';
 import { ChatModule } from './chat/chat.module';
 import { UsersModule } from './users/users.module';
 import { SentimentsModule } from './sentiments/sentiments.module';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [DbModule, ChatModule, UsersModule, SentimentsModule],
+  imports: [DbModule, ChatModule, UsersModule, SentimentsModule, StatisticsModule],
   controllers: [],
   providers: [],
 })
