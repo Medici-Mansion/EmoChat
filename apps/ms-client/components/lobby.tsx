fc79dfdafb74200c050eb3fc8bdc557b17db7e46
raymondanything <akdfhr2@gmail.com>
2023-11-10 17:14:35 +0900

    feat: 방이름 정보 db저장로직 추가

--- apps/ms-client/components/lobby.tsx
+++ apps/ms-client/components/lobby.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/lib/utils'
 import { useState } from 'react'
 import { Users } from 'lucide-react'
 import Link from 'next/link'
+import { RoomInfo } from '@/socket'
 
 // 배포환경이 Nodejs 런타임이라 Bun syntax 적용안됨. 임시데이터 추가
 const roomGroupInfo = {
@@ -60,13 +61,14 @@ const roomGroupInfo = {
 }
 
 const LobbyScreen = () => {
-  const [rooms, setRooms] = useState<{ roomId: string; roomName: string }[]>([])
+  const [rooms, setRooms] = useState<RoomInfo[]>([])
   useSocket({
     nsp: '/',
     onRoomChanged(newRooms) {
       setRooms(() => newRooms)
     },
   })
+  console.log(rooms, '<<rooms')
 
   return (
     <div
@@ -79,27 +81,23 @@ const LobbyScreen = () => {
         Group List
       </div>
       <div className="flex flex-col divide-y-[1px]">
-        {rooms.map(({ roomId, roomName }) => (
+        {rooms.map(({ id, roomName, displayName, maxUserCount, category }) => (
           <div
             className="grid grid-cols-[0.7fr_0.3fr_minmax(min-content,0.3fr)_1fr] py-5 items-center"
-            key={roomId}
+            key={id}
           >
             <h3 className="text-center text-xl font-semibold pl-4">
-              {roomGroupInfo[roomId as keyof typeof roomGroupInfo].name}
+              {displayName}
             </h3>
             <div className="flex space-x-2 text-gray-400">
               <Users />
-              <span>
-                {roomGroupInfo[roomId as keyof typeof roomGroupInfo].count}
-              </span>
+              <span>{maxUserCount}</span>
             </div>
-            <div className="text-gray-400">
-              {roomGroupInfo[roomId as keyof typeof roomGroupInfo].title}
-            </div>
+            <div className="text-gray-400">{category}</div>
             <div className="flex justify-center items-center">
               <Link
                 className="border rounded-lg px-7 h-[44px] flex items-center border-primary text-primary hover:bg-accent"
-                href={`/chat/${roomId}`}
+                href={`/chat/${id}`}
               >
                 입장
               </Link>
--- apps/ms-server/src/db/models/chat-room.model.ts
+++ apps/ms-server/src/db/models/chat-room.model.ts
@@ -1,4 +1,4 @@
-import { pgTable, text } from 'drizzle-orm/pg-core';
+import { integer, pgTable, text } from 'drizzle-orm/pg-core';
 import { CoreModel } from './core.model';
 import { relations } from 'drizzle-orm';
 import { MessageModel } from './message.model';
@@ -6,6 +6,9 @@ import { MessageModel } from './message.model';
 export const ChatRoomModel = pgTable('chatRoom', {
   ...CoreModel,
   roomName: text('roomName'),
+  category: text('category'),
+  displayName: text('displayName'),
+  maxUserCount: integer('maxUserCount'),
 });
 
 export const ChatRoomModelRelations = relations(ChatRoomModel, ({ many }) => ({
